dbc1711650f38e3f1373dc500e1b83f008bdb7b8
Monir Hossion <mdmonirhossion2002@gmail.com>
2026-09-18 23:49:14 +0600

    added to front and cdn file

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ export default {
         },
       },
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
+        sans: ['Roboto', 'Inter', 'system-ui', 'sans-serif'],
       },
     },
   },
